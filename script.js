4382eb797cc3c918b3ea76950283fd527665f8fb
Peymanrad <peymanradmo@gmail.com>
2022-05-22 20:01:14 +0430

    Selected Elements in js

---
+++ script.js
@@ -0,0 +1,5 @@
+const $ = document;
+const weightInput = $.getElementById('weight')
+const heightInput = $.getElementById('height')
+const resultPara = $.getElementById('result')
+const statusPara = $.getElementById('status')
