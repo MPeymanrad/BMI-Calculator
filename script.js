0986d38165b9127a9e276da5ab67a89156d1933b
Peymanrad <peymanradmo@gmail.com>
2022-05-22 20:31:19 +0430

    Add BMI calculating functionality

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const weightInput = $.getElementById('weight')
 const heightInput = $.getElementById('height')
 const resultPara = $.getElementById('result')
 const statusPara = $.getElementById('status')
+
+let BMINUmber;
+function calculateBMI() {
+    let weight = +weightInput.value;
+    let height = +heightInput.value / 100;
+    if (weight && height) {
+        BMINUmber = weight / (height ** 2)
+        resultPara.textContent = BMINUmber.toFixed(2)
+    } else {
+        resultPara.textContent = ''
+    }
+}
+
+weightInput.addEventListener('change',calculateBMI)
+heightInput.addEventListener('change',calculateBMI)
+weightInput.addEventListener('keyup',calculateBMI)
+heightInput.addEventListener('keyup',calculateBMI)
