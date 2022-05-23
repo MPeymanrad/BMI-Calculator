8560f5fe68b988aa9debf636994e25afb52b7792
Peymanrad <peymanradmo@gmail.com>
2022-05-23 20:39:34 +0430

    Did some little refactoring

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const heightInput = $.getElementById("height");
 const resultPara = $.getElementById("result");
 const statusPara = $.getElementById("status");
 
-const UNDERWEIGHT_COLOR = "#FFC300"
-const NORMAL_COLOR = "#9EDE73"
-const OVERWEIGHT_COLOR = "#FF5403"
-const OBESE_COLOR = "#F90716"
+const UNDERWEIGHT_COLOR = "#FFC300";
+const NORMAL_COLOR = "#9EDE73";
+const OVERWEIGHT_COLOR = "#FF5403";
+const OBESE_COLOR = "#F90716";
 
 let BMINUmber;
 function calculateBMI() {
@@ -16,27 +16,27 @@ function calculateBMI() {
   if (weight && height) {
     BMINUmber = (weight / height ** 2).toFixed(2);
     resultPara.textContent = BMINUmber;
-} else {
+  } else {
     resultPara.textContent = "";
-    statusPara.innerHTML = ''
+    statusPara.innerHTML = "";
     return;
-}
-setStatus()
+  }
+  setStatus();
 }
 function setStatus() {
-    if (BMINUmber < 18.5) {
-        statusPara.style.color = UNDERWEIGHT_COLOR
-        statusPara.textContent = 'UnderWeight'
-    } else if (30 <= BMINUmber) {
-        statusPara.style.color = OBESE_COLOR
-        statusPara.textContent = 'Obese'
-    } else if (25 <=BMINUmber && BMINUmber <=29.9) {
-        statusPara.style.color = OVERWEIGHT_COLOR
-        statusPara.textContent = 'OverWeight'
-    } else if ( 18.5 <=BMINUmber && BMINUmber <=24.9) {
-        statusPara.style.color = NORMAL_COLOR
-        statusPara.textContent = 'Normal'
-    }
+  if (BMINUmber < 18.5) {
+    statusPara.style.color = UNDERWEIGHT_COLOR;
+    statusPara.textContent = "UnderWeight";
+  } else if (BMINUmber >= 30) {
+    statusPara.style.color = OBESE_COLOR;
+    statusPara.textContent = "Obese";
+  } else if (BMINUmber >= 25 && BMINUmber <= 29.9) {
+    statusPara.style.color = OVERWEIGHT_COLOR;
+    statusPara.textContent = "OverWeight";
+  } else if (BMINUmber >= 18.5 && BMINUmber <= 24.9) {
+    statusPara.style.color = NORMAL_COLOR;
+    statusPara.textContent = "Normal";
+  }
 }
 weightInput.addEventListener("change", calculateBMI);
 heightInput.addEventListener("change", calculateBMI);
